2d11f2f44eeb011cbfe3ae970d1fd90d0f7aaf0f
Jamie Kyle <jamie.kyle@eventbrite.com>
2019-03-25 16:17:22 -0700

    fix steps

---
+++ .buildkite/setup.sh
@@ -0,0 +1,30 @@
+NEEDS_UPDATE=false
+
+# Check node/npm/bolt are on the correct versions
+if [[ ! $(node --version) =~ ^v10 ]]; then NEEDS_UPDATE=true; fi
+if [[ ! $(npm --version) =~ ^6 ]]; then NEEDS_UPDATE=true; fi
+if [[ ! $(bolt --version) =~ ^0.22 ]]; then NEEDS_UPDATE=true; fi
+
+# Check native build tools exist
+if [ ! $(command -v gcc) ]; then NEEDS_UPDATE=true; fi
+if [ ! $(command -v g++) ]; then NEEDS_UPDATE=true; fi
+if [ ! $(command -v make) ]; then NEEDS_UPDATE=true; fi
+
+if [ $NEEDS_UPDATE == true ]; then
+  apt-get update
+  apt-get install -y gnupg2
+  curl -sL https://deb.nodesource.com/setup_10.x | bash -
+  apt-get install -y nodejs gcc g++ make
+
+  npm install --global yarn
+fi
+
+echo node: $(node --version)
+echo npm: $(npm --version)
+echo yarn: $(yarn --version)
+
+echo --- :yarn: Installing Dependencies
+yarn install
+if [ $? -ne 0 ]]; then
+  echo "^^^ +++"
+fi
